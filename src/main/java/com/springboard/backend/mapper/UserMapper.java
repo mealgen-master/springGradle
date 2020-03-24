cb646a92fdc2e045037bb9d94574097a2f49a834
mealgen-master <inside1262@mealgen.net>
2020-03-24 15:18:36 +0900

    ModelStruct build.gradle 설정부분 추가

--- src/main/java/com/springboard/backend/mapper/UserMapper.java
+++ src/main/java/com/springboard/backend/mapper/UserMapper.java
@@ -2,10 +2,18 @@ package com.springboard.backend.mapper;
 
 import com.springboard.backend.dto.Users;
 import com.springboard.backend.dto.UsersDTO;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper extends EntityMapper<UsersDTO.Response , Users>{
+
+    @Mapping(source = "username", target = "username")
+    UsersDTO.Response usersToDto(Users users);
+
+    @Mapping(target = "username", ignore = true)
+    Users usersDtoTousers(UsersDTO.Response response);
 
 
 }
