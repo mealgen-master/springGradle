90b7b1a9ba870c17c3fd965514a258b79165e2b8
mealgen-master <inside1262@mealgen.net>
2020-04-09 16:42:26 +0900

    ROOT 컨트롤러 호출 부분 수정 컨트롤러 API 설명

--- src/main/java/com/springboard/backend/controller/HomeController.java
+++ src/main/java/com/springboard/backend/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.springboard.backend.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.springboard.backend.model.Greeting;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "hello world";
+    public RepresentationModel home() {
+        RepresentationModel resourceSupport = new RepresentationModel();
+        resourceSupport.add(linkTo(methodOn(UserController.class).selectUsers()).withRel("users"));
+        resourceSupport.add(linkTo(methodOn(GreetingController.class).greeting("username")).withRel("greeting"));
+        resourceSupport.add(linkTo(FileController.class).withRel("file"));
+        return resourceSupport;
     }
 }
